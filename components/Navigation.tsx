a4c0974fe6ddb5e3dc87fed346ea764adefc3231
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-25 15:19:13 -0400

    Updaing styling and add cart icon

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,10 +6,18 @@ const Navigation = () => {
       <div className="w-[80%] flex justify-between items-center m-auto">
         <div className="text-2xl">Tech Store</div>
         <div className="text-xl flex items-center gap-10">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/products"}>Products</Link>
-          <Link href={"/categories"}>Categories</Link>
-          <Link href={"/deals"}>Deals</Link>
+          <Link className="nav-link" href={"/"}>
+            Home
+          </Link>
+          <Link className="nav-link" href={"/products"}>
+            Products
+          </Link>
+          <Link className="nav-link" href={"/categories"}>
+            Categories
+          </Link>
+          <Link className="nav-link" href={"/deals"}>
+            Deals
+          </Link>
           <div className="relative">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -33,7 +41,28 @@ const Navigation = () => {
             />
           </div>
         </div>
-        <div className="text-xl">Login/Register</div>
+        <div className="text-xl flex items-end gap-4">
+          <div className="relative">
+            <p className="bg-red-600 text-center w-5 h-5 text-base font-semibold rounded-[50%] leading-none absolute -top-2 left-3">
+              0
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </div>
+          <p>Login/Register</p>
+        </div>
       </div>
     </div>
   );
