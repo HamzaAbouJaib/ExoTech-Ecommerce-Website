9b28111c92f0b317b7cfaa606d078525c65062bc
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-25 15:05:21 -0400

    Add search icon to search bar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -10,11 +10,28 @@ const Navigation = () => {
           <Link href={"/products"}>Products</Link>
           <Link href={"/categories"}>Categories</Link>
           <Link href={"/deals"}>Deals</Link>
-          <input
-            type="text"
-            placeholder="Enter your keywords..."
-            className="px-4 py-1 rounded-[2rem] text-lg"
-          />
+          <div className="relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-4 h-4 absolute top-[50%] translate-y-[-50%] left-3 text-slate-800"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+
+            <input
+              type="text"
+              placeholder="Enter your keywords..."
+              className="pl-10 pr-4 py-1 rounded-[2rem] text-lg"
+            />
+          </div>
         </div>
         <div className="text-xl">Login/Register</div>
       </div>
