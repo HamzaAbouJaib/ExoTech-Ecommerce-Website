9f715a4cadea07ba035f7174052502826f5721c0
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-25 20:25:01 -0400

    Update image border radius

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -6,7 +6,7 @@ const ProductCard = ({ name, brand, price, images, discount }: ProductType) => {
   return (
     <div className="flex flex-col w-[500px] justify-between bg-opacity-50 rounded-[1.5rem] hover:bg-transparent hover:border-gray-900 hover:border-opacity-75 transition-all duration-500 ease-in-out">
       <div className="pb-5 flex flex-col gap-3">
-        <Link href={"/"} className="h-56 w-full bg-gray-100 flex justify-center rounded-t-[1.5rem]">
+        <Link href={"/"} className="h-56 w-full bg-gray-100 flex justify-center rounded-[1rem]">
           <img className="h-full" src={images?.[0]} alt="Project Image" />
         </Link>
         <div className="grid grid-cols-2">
