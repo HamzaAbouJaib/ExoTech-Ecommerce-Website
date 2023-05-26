c2efc619628ce71aaea18840a2ee038410948a0e
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-25 20:23:15 -0400

    Update styling

--- components/TodaysDeals.tsx
+++ components/TodaysDeals.tsx
@@ -7,7 +7,7 @@ const TodaysDeals = ({ newDeals }: { newDeals: ProductType[] }) => {
       <h1 className="text-3xl font-semibold mb-7">
         Todays Best Deals For You!
       </h1>
-      <div>
+      <div className="grid grid-cols-3 gap-20">
         {newDeals.map((deal) => (
           <ProductCard {...deal} />
         ))}
