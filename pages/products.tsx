c94607b7e1617a7785fa7ad287cf35d7f9515401
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-25 21:21:25 -0400

    Create a page to display all products

---
+++ pages/products.tsx
@@ -0,0 +1,15 @@
+import Navigation from "@/components/Navigation";
+
+export default function products() {
+  return (
+    <>
+      <Navigation />
+      <div className="w-[80%] m-auto mb-10 pt-32">
+        <h1 className="text-3xl font-semibold mb-7">All Products</h1>
+      </div>
+    </>
+  );
+}
+
+
+
