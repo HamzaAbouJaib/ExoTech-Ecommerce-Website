2c38cfaacc11ff81d58d314ddd7969f6dc79ce64
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-25 21:30:10 -0400

    Create categories page

---
+++ pages/categories.tsx
@@ -0,0 +1,12 @@
+import Navigation from "@/components/Navigation";
+
+export default function categories() {
+  return (
+    <>
+      <Navigation />
+      <div className="w-[80%] m-auto mb-10 pt-20">
+        
+      </div>
+    </>
+  );
+}
