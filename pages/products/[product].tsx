19862e1c20a4082127b23f71774b5dacee6f8e14
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-28 14:47:42 -0400

    Create individual product page

---
+++ pages/products/[product].tsx
@@ -0,0 +1,10 @@
+import Navigation from "@/components/Navigation";
+
+export default function ProductPage() {
+  return (
+    <>
+      <Navigation />
+      <div className="w-[80%] m-auto mb-10 pt-20">Product Page</div>
+    </>
+  );
+}
