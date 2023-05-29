a79f2e99a4441788255f38f5f4d72c9efd0e348d
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-28 22:33:58 -0400

    Add ability to select an image

--- pages/products/[product].tsx
+++ pages/products/[product].tsx
@@ -2,27 +2,39 @@ import Navigation from "@/components/Navigation";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 import ProductType from "@/types/ProductType";
+import { useState } from "react";
 
 export default function ProductPage({ product }: { product: ProductType }) {
+  const [activeImage, setActiveImage] = useState(product?.images?.[0]);
+
+  const activeStyling = "bg-gray-200/70";
+
   return (
     <>
       <Navigation />
       <div className="w-[80%] m-auto mb-10 pt-20">
         <div className="mt-32 grid grid-cols-2 gap-32">
-          <div className="flex">
-            <div className="w-32">
+          <div className="flex items-center gap-10">
+            <div className="flex flex-col gap-4">
               {product?.images?.map((img) => (
-                <img
-                  className="cursor-pointer"
-                  src={img}
-                  alt={product?.name + " image"}
-                />
+                <div
+                  className={`w-32 p-3 rounded-lg flex justify-center ${
+                    img === activeImage && activeStyling
+                  }`}
+                  onClick={() => setActiveImage(img)}
+                >
+                  <img
+                    className="cursor-pointer"
+                    src={img}
+                    alt={product?.name + " image"}
+                  />
+                </div>
               ))}
             </div>
-            <div className="w-[500px] place-self-end">
+            <div className="w-[500px] h-[500px] p-10 flex items-center">
               <img
-                className="w-full"
-                src={product?.images?.[0]}
+                className=""
+                src={activeImage}
                 alt={product?.name + " image"}
               />
             </div>
