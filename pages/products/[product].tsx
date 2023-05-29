9ccc9aacf8e30a02a7f4275c4ab73363684ebfd8
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-28 22:20:10 -0400

    Display images

--- pages/products/[product].tsx
+++ pages/products/[product].tsx
@@ -1,14 +1,32 @@
 import Navigation from "@/components/Navigation";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import ProductType from "@/types/ProductType";
 
-export default function ProductPage() {
+export default function ProductPage({ product }: { product: ProductType }) {
   return (
     <>
       <Navigation />
       <div className="w-[80%] m-auto mb-10 pt-20">
         <div className="mt-32 grid grid-cols-2 gap-32">
-          <div>Image</div>
+          <div className="flex">
+            <div className="w-32">
+              {product?.images?.map((img) => (
+                <img
+                  className="cursor-pointer"
+                  src={img}
+                  alt={product?.name + " image"}
+                />
+              ))}
+            </div>
+            <div className="w-[500px] place-self-end">
+              <img
+                className="w-full"
+                src={product?.images?.[0]}
+                alt={product?.name + " image"}
+              />
+            </div>
+          </div>
           <div>Info</div>
         </div>
       </div>
