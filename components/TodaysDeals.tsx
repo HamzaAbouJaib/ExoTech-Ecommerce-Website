65717cf7de1c7e6dc3bbb6761fe46bb1296335c8
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-29 22:18:39 -0400

    Fix spelling

--- components/TodaysDeals.tsx
+++ components/TodaysDeals.tsx
@@ -5,7 +5,7 @@ const TodaysDeals = ({ newDeals }: { newDeals: ProductType[] }) => {
   return (
     <div className="mt-20">
       <h1 className="text-3xl font-semibold mb-7">
-        Todays Best Deals For You!
+        Today's Best Deals For You!
       </h1>
       <div className="grid grid-cols-3 gap-10">
         {newDeals.map((deal) => (
