fb8146d445ba86dc6558bf63b7cbe940eafd7d1d
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-29 22:19:19 -0400

    Link to individual product page

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -3,19 +3,26 @@ import Link from "next/link";
 import React from "react";
 import CardPrice from "./CardPrice";
 
-const ProductCard = ({ name, brand, price, images, discount }: ProductType) => {
+const ProductCard = ({
+  _id,
+  name,
+  brand,
+  price,
+  images,
+  discount,
+}: ProductType) => {
   return (
     <div className="flex flex-col justify-between">
       <div className="pb-5 flex flex-col gap-3">
         <Link
-          href={"/"}
+          href={"/products/" + _id}
           className="h-56 w-full bg-gray-100 flex justify-center rounded-[1rem]"
         >
           <img className="h-full" src={images?.[0]} alt="Project Image" />
         </Link>
         <div className="grid grid-cols-2">
           <div>
-            <Link href={"/"} className="text-2xl font-bold">
+            <Link href={"/products/" + _id} className="text-2xl font-bold">
               {name}
             </Link>
             <p>{brand}</p>
