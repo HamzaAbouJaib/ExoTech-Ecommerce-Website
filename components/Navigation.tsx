20c15520ebb6625f6997d43a7226f31684e10e77
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-05-31 12:29:36 -0400

    Add text to cart nav item

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -41,8 +41,9 @@ const Navigation = () => {
             />
           </div>
         </div>
-        <div className="text-xl flex items-end gap-4">
-          <Link href={"/cart"} className="relative">
+        <div className="text-xl flex items-end gap-6">
+          <p>Login/Register</p>
+          <Link href={"/cart"} className="relative flex items-center gap-2">
             <span className="bg-red-600 flex justify-center items-center w-5 h-5 text-base font-semibold rounded-[50%] leading-none absolute -top-2 left-3">
               0
             </span>
@@ -60,8 +61,8 @@ const Navigation = () => {
                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
               />
             </svg>
+            Cart
           </Link>
-          <p>Login/Register</p>
         </div>
       </div>
     </div>
