809c720b0e984215236f50f08aa5c34700caa0c8
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-01 22:13:10 -0400

    Update webhooks to get shipping address from checkout session

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -30,15 +30,24 @@ export default async function handler(
     case "checkout.session.completed":
       const data = event.data.object;
       const orderId = data.metadata.orderId;
+      const { city, country, line1, postal_code, state } =
+        data.shipping_details.address;
       const paid = data.payment_status === "paid";
       if (orderId && paid) {
-        await Order.findByIdAndUpdate(orderId, { paid: true });
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+          address: line1,
+          city,
+          state,
+          country,
+          postalCode: postal_code,
+        });
       }
       break;
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
-  
+
   res.status(200).send("ok");
 }
 
