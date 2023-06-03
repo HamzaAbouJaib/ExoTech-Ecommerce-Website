036de1291b49d8db3c41614f452f3ebd082777ad
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-03 19:02:55 -0400

    Create personal info section

---
+++ components/PersonalInfo.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import AccountForm from "./AccountForm";
+
+type CustomerType = {
+  _id: string;
+  name: string;
+  email: string;
+  mobile: string;
+};
+
+const PersonalInfo = () => {
+  const { data: session } = useSession();
+  const [customer, setCustomer] = useState<CustomerType>();
+
+  useEffect(() => {
+    if (!session) return;
+    axios.get("/api/customers?email=" + session.user.email).then((response) => {
+      setCustomer(response.data);
+    });
+  }, [session]);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({ defaultValues: customer });
+
+  return (
+    <div className="text-lg">
+      <h1 className="text-2xl font-semibold leading-none">
+        Personal Information
+      </h1>
+      <p className="text-gray-600 mb-2">Manage you personal information</p>
+      {customer && (
+        <AccountForm preloaded={customer} />
+      )}
+    </div>
+  );
+};
+
+export default PersonalInfo;
