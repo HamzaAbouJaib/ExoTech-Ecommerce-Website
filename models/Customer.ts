45b84f035e76e42d697fdae04201f769f72ab9b3
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-03 19:00:50 -0400

    Add mobile field

--- models/Customer.ts
+++ models/Customer.ts
@@ -4,6 +4,7 @@ const CustomerSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    mobile: String,
     password: { type: String, required: true },
   },
   { timestamps: true }
