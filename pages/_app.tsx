e6cd0cdba7fbecdd793be5adc45211a927679bec
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-03 13:07:58 -0400

    Wrap app in session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import { CartContextProvider } from "@/store/CartContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <CartContextProvider>
-      <Component {...pageProps} />
-    </CartContextProvider>
+    <SessionProvider session={session}>
+      <CartContextProvider>
+        <Component {...pageProps} />
+      </CartContextProvider>
+    </SessionProvider>
   );
 }
