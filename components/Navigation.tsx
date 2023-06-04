35f798ccdfbcc1e33539dac7575e27e9b0cfb47d
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-04 16:59:04 -0400

    Add company name and logo and make it link to home page

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -10,7 +10,23 @@ const Navigation = () => {
   return (
     <div className="fixed bg-slate-900 w-full p-6 text-white shadow-md z-10">
       <div className="w-[80%] flex justify-between items-center m-auto">
-        <div className="text-2xl">Tech Store</div>
+        <Link href={"/"} className="text-2xl flex items-center gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 002.25-2.25V6.75a2.25 2.25 0 00-2.25-2.25H6.75A2.25 2.25 0 004.5 6.75v10.5a2.25 2.25 0 002.25 2.25zm.75-12h9v9h-9v-9z"
+            />
+          </svg>
+          ExoTech
+        </Link>
         <div className="text-xl flex items-center gap-10">
           <Link className="nav-link" href={"/"}>
             Home
