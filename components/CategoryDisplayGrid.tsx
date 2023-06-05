b0b0412627a62dfc4801ff251f79fc4e98a45c75
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-05 10:24:47 -0400

    Make category display grid responsive

--- components/CategoryDisplayGrid.tsx
+++ components/CategoryDisplayGrid.tsx
@@ -96,8 +96,16 @@ const CategoryDisplayGrid = ({
 
   return (
     <div className="mt-20">
-      <div className="flex justify-between">
-        <h1 className="text-3xl font-semibold mb-7">{category?.name}</h1>
+      <div className="flex">
+        <div className="flex gap-4 items-baseline w-full">
+          <h1 className="text-3xl font-semibold mb-7">{category?.name} </h1>
+          {link && (
+            <Link className="text-lg text-primary underline" href={link}>
+              View More
+            </Link>
+          )}
+        </div>
+
         {!link && (
           <div className="flex gap-5">
             {category?.properties.map((p) => (
@@ -145,32 +153,10 @@ const CategoryDisplayGrid = ({
           </div>
         )}
       </div>
-      <div className="grid grid-cols-4 gap-10">
+      <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-10">
         {products?.map((product) => (
           <ProductCard {...product} />
         ))}
-        {link && (
-          <Link
-            href={link}
-            className="bg-slate-200 text-3xl font-semibold flex justify-center items-center gap-2 rounded-[1.5rem] hover:bg-slate-300 duration-500"
-          >
-            Show All
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={3}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-              />
-            </svg>
-          </Link>
-        )}
       </div>
     </div>
   );
