f49d7d6bd514f8136417a362ecf33a3581930d62
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-05 21:22:31 -0400

    Fix products not being categorized correctly

--- lib/getCategoryProducts.ts
+++ lib/getCategoryProducts.ts
@@ -3,41 +3,42 @@ import { mongooseConnect } from "./mongoose";
 import { Product } from "@/models/Product";
 
 export async function getCategoryProducts() {
-    await mongooseConnect();
-    const categoryProducts: any[] = [];
-    const categories = await Category.find();
-    const allProducts = await Product.find({ madeByGuest: { $eq: false } });
-  
-    for (const category of categories) {
-      const products = [];
-      let selectedCategory = category;
-      products.push(
-        ...allProducts.filter((p) => p.category.equals(selectedCategory._id))
+  await mongooseConnect();
+  const categoryProducts: any[] = [];
+  const categories = await Category.find();
+  const allProducts = await Product.find({ madeByGuest: { $eq: false } });
+
+  for (const category of categories) {
+    const products = [];
+    let selectedCategory = category;
+    products.push(
+      ...allProducts.filter((p) => p.category.equals(selectedCategory._id))
+    );
+    while (selectedCategory?.parent?._id) {
+      const parentCategory = categories.find(({ _id }) =>
+        _id.equals(selectedCategory.parent._id)
       );
-      while (selectedCategory?.parent?._id) {
-        const parentCategory = categories.find(({ _id }) =>
-          _id.equals(selectedCategory.parent._id)
-        );
-  
-        products.push(
-          ...allProducts.filter((p) => p.category.equals(parentCategory?._id))
-        );
-        selectedCategory = parentCategory;
-      }
-  
-      let add = true;
-  
-      for (const categoryProduct of categoryProducts) {
-        if (categoryProduct.category.equals(selectedCategory)) {
-          categoryProduct.products = [...products];
-          add = false;
-          break;
-        }
-      }
-  
-      if (add) {
-        categoryProducts.push({ category: selectedCategory, products: products });
+
+      products.push(
+        ...allProducts.filter((p) => p.category.equals(parentCategory?._id))
+      );
+      selectedCategory = parentCategory;
+    }
+
+    let add = true;
+
+    for (const categoryProduct of categoryProducts) {
+      if (categoryProduct.category.equals(selectedCategory)) {
+        categoryProduct.products.push(...products);
+        add = false;
+        break;
       }
     }
-    return categoryProducts;
+    console.log(categoryProducts[0]?.products);
+
+    if (add) {
+      categoryProducts.push({ category: selectedCategory, products: products });
+    }
   }
+  return categoryProducts;
+}
