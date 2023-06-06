367ca7d3d0bc11928761c790b1af98c24b5d5c88
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-05 20:54:54 -0400

    Update image styling

--- components/ProductImages.tsx
+++ components/ProductImages.tsx
@@ -40,9 +40,9 @@ const ProductImages = ({
   // }
 
   return (
-    <div className="flex flex-col lg:gap-20 gap-10 max-lg:order-2">
-      <div className="lg:w-[500px] w-full h-[300px] flex items-start justify-center">
-        <img src={activeImage} alt={name + " image"} title={name} className="lg:h-[100%] w-[100%]" />
+    <div className="flex flex-col lg:gap-20 gap-10 max-lg:order-2 items-center">
+      <div className="h-[300px] flex items-center justify-center">
+        <img src={activeImage} alt={name + " image"} title={name} className="h-full" />
       </div>
       <div className="lg:w-[500px] w-[350px] flex items-center justify-center gap-2">
         <button
