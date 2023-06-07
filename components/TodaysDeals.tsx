5b4b7e3ea67f16aaa1b14495a4f2bb2d16f720e1
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-06-06 20:27:27 -0400

    Fix quote

--- components/TodaysDeals.tsx
+++ components/TodaysDeals.tsx
@@ -5,7 +5,7 @@ const TodaysDeals = ({ newDeals }: { newDeals: ProductType[] }) => {
   return (
     <div className="mt-20">
       <h1 className="text-3xl font-semibold mb-7">
-        Today's Best Deals For You!
+        Today&apos;s Best Deals For You!
       </h1>
       <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-10">
         {newDeals.map((deal) => (
